48e98633d242fb08c075d68b615e08853d920e79
yangtm <515872372@qq.com>
2015-09-07 13:11:31 +0800

    2

--- app/src/main/java/com/yibu/kuaibu/MainActivity.java
+++ app/src/main/java/com/yibu/kuaibu/MainActivity.java
@@ -29,8 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
+
 
         return super.onOptionsItemSelected(item);
     }
